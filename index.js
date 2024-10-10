6d5f46d5d0914ccd392f7cc89ea60dff794accb4
Dmitry Fedorov <95097887+fedorovvdmitry@users.noreply.github.com>
2024-10-10 20:13:59 +0300

    UPD btn presentation

--- index.js
+++ index.js
@@ -81,7 +81,10 @@ if (!self.__WB_pmw) {
 
     if (presentationBtn) {
       presentationBtn.addEventListener('click', function () {
-        window.open('docs/presentation.pdf', '_blank')
+        window.open(
+          'https://sibstek.ru/files/%D0%9F%D1%80%D0%B5%D0%B7%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F%20%D0%B4%D0%BB%D1%8F%20%D0%B8%D0%BD%D0%B2%D0%B5%D1%81%D1%82%D0%BE%D1%80%D0%BE%D0%B2.pdf',
+          '_blank'
+        )
       })
     }
 
