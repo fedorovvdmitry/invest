4489b20ffec4ac3eaa8703f75967be2f339b739f
Dmitry Fedorov <95097887+fedorovvdmitry@users.noreply.github.com>
2024-10-11 11:28:51 +0300

    UPD index.html

--- index.js
+++ index.js
@@ -77,16 +77,16 @@ if (!self.__WB_pmw) {
         })
     }
 
-    var presentationBtn = document.getElementById('presentation-btn')
+    // var presentationBtn = document.getElementById('presentation-btn')
 
-    if (presentationBtn) {
-      presentationBtn.addEventListener('click', function () {
-        window.open(
-          'https://sibstek.ru/files/%D0%9F%D1%80%D0%B5%D0%B7%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F%20%D0%B4%D0%BB%D1%8F%20%D0%B8%D0%BD%D0%B2%D0%B5%D1%81%D1%82%D0%BE%D1%80%D0%BE%D0%B2.pdf',
-          '_blank'
-        )
-      })
-    }
+    // if (presentationBtn) {
+    //   presentationBtn.addEventListener('click', function () {
+    //     window.open(
+    //       'https://inform.uscapital.ru/sibsteklo/Prezentaciya_investoram.pdf',
+    //       '_blank'
+    //     )
+    //   })
+    // }
 
     // phone inputs
     var phoneInputs = document.querySelectorAll('[data-role="phone/input"]')
